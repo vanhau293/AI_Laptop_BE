bf788939859dbef209cb5e9c920b5d15c15975ba
hauchauvan <hau.chauvan@unified.com>
2023-10-11 09:50:39 +0700

    "update to deploy"

--- src/main/java/com/example/demo/bean/CorsConfiguration.java
+++ src/main/java/com/example/demo/bean/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000/", "https://ai-laptop.vercel.app/");
             }
         };
     }
